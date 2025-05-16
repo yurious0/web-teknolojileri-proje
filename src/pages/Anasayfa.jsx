e73710d818fcdd8868391dca7992dba71ae56859
yuri <poyrazfe0@gmail.com>
2025-05-16 23:31:20 +0300

    dosya bozulduğundan yeni dosya oluşturuldu anasayfa içerikleri planlandı

--- src/pages/Anasayfa.jsx
+++ src/pages/Anasayfa.jsx
@@ -4,10 +4,10 @@ function Anasayfa() {
       <h1>Hakkımda</h1>
       <h2 style={{ marginTop: '32px', marginBottom: '8px' }}>Merhaba</h2>
       <p>
-        Ben Poyraz Efe Özcan, Sakarya Üniversitesi Bilgisayar Mühendisliği 1. sınıf öğrencisiyim.
+        Ben Poyraz Efe Özcan Sakarya Üniversitesi Bilgisayar Mühendisliği 1. sınıf öğrencisiyim.
         Bu siteyi 2024-2025 döneminde Web Teknolojileri dersinin projesi için geliştirdim.
       </p>
-      <p style={{ marginBottom: '2px' }}>
+      <p style={{ marginTop: '8px' }}>
         Bu sitede özgeçmişim, yaşadığım şehir, kültürel mirasımız, ilgi alanlarım ve iletişim
         bilgilerim gibi bölümleri bulabilirsiniz.
       </p>
